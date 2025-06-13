78872b83707ba461ac2c6c9b7da5e25de3f4312b
Russell Rundell <83023898+RussellR-MSFT@users.noreply.github.com>
2025-06-13 16:52:39 -0500

    updating to make curl output basic

--- index.php
+++ index.php
@@ -10,74 +10,104 @@
     <title>PHP Origin Tools</title>
     <link rel="stylesheet" href="styles.css">
   </head>
-  <body>
-    <div class="main_page">
-      <div class="page_header floating_element">
-       <img src="/tools.png" alt="tools image" class="floating_element"/> 
-        <span class="floating_element">
-          PHP Origin Tools</br>
-          <?php echo date(DATE_RFC2822);?>
-        </span>
-      </div>
-      <div class="content_section floating_element">
-        <div class="section_header section_header_red">
-          <div id="about"></div>
-          Host name: <?php echo gethostname(); ?>
-        </div>
-        <div class="content_section_text">
-          <p>
-            This page provides some helpful tools to reflect headers and other data that is sent via GET requests.  This helps in
-            troubleshooting application layer issues by providing visibility into the headers seen on the backend or origin behind various
-            networking load balancers such as Azure Application Gateways, Content Delivery Network endpoints, or Azure Front Doors.
-          </p>
-        </div>
-        <div class="section_header">
-            <div id="querystrings"></div>
-            Query strings
-        </div>
-        <div class="content_section_text">
-            <?php
-            $queryString = $_SERVER['QUERY_STRING'];
-            echo "Raw query string: $queryString";
-            ?>
-        </div>
-        <div class="section_header">
-            <div id="requestheaders"></div>
-            Request Headers
-        </div>
-        <div class="content_section_text">
-          <?php
-           # From https://www.php.net/manual/en/function.apache-request-headers.php
-           $requestHeaders = apache_request_headers();
+  <?php
+  $userAgent = $_SERVER['HTTP_USER_AGENT'] ?? '';
+  $isCurl = stripos($userAgent, 'curl') !== false;
+  ?>
 
-           foreach ($requestHeaders as $header => $value) {
-            echo "$header: $value <br />\n";
-           }
-          ?>
+  <?php if ($isCurl): ?>
+    <body>
+      <pre>
+  PHP Origin Tools (Basic Output)
+
+  Host name: <?php echo gethostname(); ?>
+
+
+  Raw query string: <?php echo $_SERVER['QUERY_STRING']; ?>
+
+
+  Request Headers:
+  <?php
+  $requestHeaders = function_exists('apache_request_headers') ? apache_request_headers() : [];
+  foreach ($requestHeaders as $header => $value) {
+      echo "$header: $value\n";
+  }
+  ?>
+
+
+  POST Values:
+  <?php print_r($_POST); ?>
+
+
+      </pre>
+    </body>
+  <?php else: ?>
+    <body>
+      <div class="main_page">
+        <div class="page_header floating_element">
+         <img src="/tools.png" alt="tools image" class="floating_element"/> 
+          <span class="floating_element">
+            PHP Origin Tools</br>
+            <?php echo date(DATE_RFC2822);?>
+          </span>
         </div>
-        <div class="section_header">
-            <div id="postvalues"></div>
-            POST Values
-            </div>
-        <div class="content_section_text">
-          <?php 
-           # From https://stackoverflow.com/questions/196664/print-out-post-values
-           echo "<pre>"; print_r($_POST) ;  echo "</pre>";  
-          ?>
-        </div>
-        <div class="section_header">
-           <div id="websocketsdemolink"></div>
-           Websockets Demo
-        </div>
-        <div class="content_section_text">
-           <a href="websocketsdemo.php">Websockets Demo Chat Room</a>
-           <p>
-              Provides a quick easy way to see websockets in action.  Requires a separate websockets server.
-           </p>
+        <div class="content_section floating_element">
+          <div class="section_header section_header_red">
+            <div id="about"></div>
+            Host name: <?php echo gethostname(); ?>
+          </div>
+          <div class="content_section_text">
+            <p>
+              This page provides some helpful tools to reflect headers and other data that is sent via GET requests.  This helps in
+              troubleshooting application layer issues by providing visibility into the headers seen on the backend or origin behind various
+              networking load balancers such as Azure Application Gateways, Content Delivery Network endpoints, or Azure Front Doors.
+            </p>
+          </div>
+          <div class="section_header">
+              <div id="querystrings"></div>
+              Query strings
+          </div>
+          <div class="content_section_text">
+              <?php
+              $queryString = $_SERVER['QUERY_STRING'];
+              echo "Raw query string: $queryString";
+              ?>
+          </div>
+          <div class="section_header">
+              <div id="requestheaders"></div>
+              Request Headers
+          </div>
+          <div class="content_section_text">
+            <?php
+             $requestHeaders = function_exists('apache_request_headers') ? apache_request_headers() : [];
+             foreach ($requestHeaders as $header => $value) {
+              echo "$header: $value <br />\n";
+             }
+            ?>
+          </div>
+          <div class="section_header">
+              <div id="postvalues"></div>
+              POST Values
+              </div>
+          <div class="content_section_text">
+            <?php 
+             echo "<pre>"; print_r($_POST) ;  echo "</pre>";  
+            ?>
+          </div>
+          <div class="section_header">
+             <div id="websocketsdemolink"></div>
+             Websockets Demo
+          </div>
+          <div class="content_section_text">
+             <a href="websocketsdemo.php">Websockets Demo Chat Room</a>
+             <p>
+                Provides a quick easy way to see websockets in action.  Requires a separate websockets server.
+             </p>
+          </div>
         </div>
       </div>
-    </div>
-    <div class="validator">
-    </div>
-  </body>
+      <div class="validator">
+      </div>
+    </body>
+  <?php endif; ?>
 </html>          
